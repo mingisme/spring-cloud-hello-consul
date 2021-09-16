bf6b31e10baeaf2b6dcb6b30c32e9c1185ce3e98
ming.wang4 <ming.wang4@envision-digital.com>
2021-09-16 23:05:00 +0800

    init consul config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-cloud-hello-consul
+
+
+###add kv in consul
+/config/consul-client-app/useLocalCache = false
---
+++ src/test/java/com/example/springcloudhelloconsul/SpringCloudHelloConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudhelloconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHelloConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
